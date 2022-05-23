7e9b06151cfc6568883033381204bd6ebf2028d9
Nahor-irup <rohanpuri195@gmail.com>
2022-05-23 12:04:00 +0545

    day3

---
+++ day3/index.js
@@ -0,0 +1,16 @@
+console.log('HELLO')
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(2, 3))
+
+global.myName = "ROHAN"
+
+function print() {
+    console.log(myName)
+}
+print();
+
+setInterval(() => console.log("Printed after 2sec"), 2000)
