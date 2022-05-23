324c34fda720173a4b298f7cb764a6f929ffc19b
Nahor-irup <rohanpuri195@gmail.com>
2022-05-23 14:47:52 +0545

    day2

--- day2/app.js
+++ day2/app.js
@@ -1,1 +1,8 @@
 console.log("hello from app.js")
+// export function add(a, b) {
+//     return a + b;
+// }
+
+// export function sub(a, b) {
+//     return a > b ? a - b : b - a;
+// }
--- day2/index.js
+++ day2/index.js
@@ -15,6 +15,32 @@
 
 // setInterval(() => console.log("Printed after 2sec"), 2000)
 
+
+// const { add, sub } = require("./app");
+
+import { add } from "./app.js";
+import { sub } from "./app.js"
+
+console.log(add(2, 1));
+console.log(sub(2, 1));
+
+console.log('HELLO')
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(2, 3))
+
+myName = "ROHAN"
+
+function print() {
+    console.log(myName)
+}
+print();
+
+setInterval(() => console.log("Printed after 2sec"), 2000)
+
 console.log(__filename)
 console.log(__dirname)
 
